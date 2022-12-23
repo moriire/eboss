ffd14f01a732be00f6087c78b607074e3123c443
Mobolaji124 <ibraheemabdulsalam@gmail.com>
2022-12-23 10:29:37 +0100

    cont edited

--- static/dist/src/app/js/app.js
+++ static/dist/src/app/js/app.js
@@ -772,7 +772,7 @@ module.exports = __webpack_require__.p + "img/round.png";
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d7806362091ed1ef"; }
+/******/ 		__webpack_require__.h = function() { return "3448450309c117dd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
