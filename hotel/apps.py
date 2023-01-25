93e96bc84dfe485b7b13abe45b6e86990be35354
Mobolaji124 <ibraheemabdulsalam@gmail.com>
2023-01-25 10:57:37 +0100

    latest

--- hotel/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class BookingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hotel'
-    def ready(self):
-        import hotel.signal
